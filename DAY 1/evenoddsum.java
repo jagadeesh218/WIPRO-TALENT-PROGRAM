d79f1339b1491f9c6a3be8b7505605be124b6b00
jagadeesh218 <90845196+jagadeesh218@users.noreply.github.com>
2023-02-07 10:04:02 +0530

    Create evenoddsum.java

---
+++ DAY 1/evenoddsum.java
@@ -0,0 +1,27 @@
+package jagadeesh;
+
+import java.util.Scanner;
+
+public class evenodd 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("ENTER THE FIRST NUMBER :");
+	Scanner scanner = new Scanner(System.in);
+	int a = scanner.nextInt();
+	System.out.println("ENTER THE SECOND NUMBER :");
+	int b = scanner.nextInt();
+	int sum =a+b;
+
+
+	if(sum%2==0)
+	{
+	System.out.println("THE "+sum +" NUMBER EVEN");
+	}
+	else 
+	{
+	System.out.println("THE "+sum +" NUMBER ODD");
+	}
+	}
+	}
