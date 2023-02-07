82062b068e028dcd3608d404be0344985ee936b6
jagadeesh218 <90845196+jagadeesh218@users.noreply.github.com>
2023-02-07 09:59:32 +0530

    Create onetohundred.java

---
+++ DAY 1/onetohundred.java
@@ -0,0 +1,18 @@
+public class onetohundred {
+
+	public onetohundred() {
+	
+	}
+
+	public static void main(String[] args) 
+	{
+		int number=100;
+		for(int i=1;i<=number;i++)
+		{
+		System.out.println(i);
+		}
+		
+
+	}
+
+}
