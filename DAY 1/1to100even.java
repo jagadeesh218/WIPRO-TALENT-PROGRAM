abf9512a88d1338b0ac69cad7d37b2bbbf951d45
jagadeesh218 <90845196+jagadeesh218@users.noreply.github.com>
2023-02-07 10:00:13 +0530

    Create 1to100even.java

---
+++ DAY 1/1to100even.java
@@ -0,0 +1,16 @@
+public class onetohundredeven
+{
+public static void main(String[] args)
+{
+	 int number=100;  
+
+	 for (int i=0; i<=number; i++)   
+	 {  
+	 if (i%2==0)   
+	 {  
+	 System.out.println(i + " ");  
+	 }  
+	 }  
+			}
+
+}
