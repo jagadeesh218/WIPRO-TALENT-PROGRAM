5aa761e9d5b110db0ba4fc340bc0002d412cc6a0
jagadeesh218 <90845196+jagadeesh218@users.noreply.github.com>
2023-02-13 11:55:51 +0530

    Create Stdin and Stdout II.java

---
+++ DAY 7/HACKER RANK   SOLUTION/Stdin and Stdout II.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner scan = new Scanner(System.in);
+
+    int i = scan.nextInt();
+    double d = scan.nextDouble();
+    scan.nextLine();
+    String s = scan.nextLine();
+    // Write your code here.
+
+    System.out.println("String: " + s);
+    System.out.println("Double: " + d);
+    System.out.println("Int: " + i);
+
+
+}
+    }
+input(stdin)
+100
+42
+42 is the answer to life, the universe, and everything!
+  
+  output(stdout)
+String: 42 is the answer to life, the universe, and everything!
+double: 42.0
+int: 100
